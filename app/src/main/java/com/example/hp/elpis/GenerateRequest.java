07db2db243f56b8d595a89089595f2ccae88f20b
yash-ag <agarwal.yash3101@gmail.com>
2018-10-19 00:30:50 +0530

    Connect to Azure

--- app/src/main/java/com/example/hp/elpis/GenerateRequest.java
+++ app/src/main/java/com/example/hp/elpis/GenerateRequest.java
@@ -1,23 +1,39 @@
 package com.example.hp.elpis;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
+import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
+import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
+
+import java.net.MalformedURLException;
+
 public class GenerateRequest extends AppCompatActivity {
 
+    private static final String TAG = GenerateRequest.class.getName();
     private EditText mLocationEdit;
     private EditText mNumberOfPeopleEdit;
     private EditText mCommentEdit;
     private CheckBox mDoctorRequiredCheck;
     private Button mSendRequest;
+
+    private HelpRequestDetails help = null;
+    private MobileServiceClient mClient;
+    private MobileServiceTable<HelpRequestDetails> mHelpRequestTable;
+
+
     private View.OnClickListener mRequestListener = new View.OnClickListener() {
         public void onClick(View v) {
-            insertData();
+            getData();
+            addData();
         }
     };
 
@@ -28,7 +44,8 @@ public class GenerateRequest extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Send Request");
-        
+
+        connectAzure();
         mLocationEdit = (EditText) findViewById(R.id.location);
         mNumberOfPeopleEdit = (EditText) findViewById(R.id.number_of_people);
         mDoctorRequiredCheck = (CheckBox) findViewById(R.id.doctor_required);
@@ -39,12 +56,48 @@ public class GenerateRequest extends AppCompatActivity {
 
     }
 
-    public void insertData() {
+    // This connects the app to Azure Infrastructure
+    public void connectAzure() {
+        try {
+            mClient = new MobileServiceClient(
+                    "https://elpisiitr.azurewebsites.net",
+                    this
+            );
+            mHelpRequestTable = mClient.getTable("Help_required_details", HelpRequestDetails.class);
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "connectAzure: Error connecting to table ");
+        }
+    }
+
+    //Fetches the user entered Data from the text fields
+    public void getData() {
         String location = mLocationEdit.getText().toString().trim();
         int numberOfPeople = Integer.parseInt(mNumberOfPeopleEdit.getText().toString().trim());
         boolean doctorRequired = mDoctorRequiredCheck.isSelected();
         String comment = mCommentEdit.getText().toString().trim();
 
-        HelpRequestDetails help = new HelpRequestDetails(location, numberOfPeople, doctorRequired, comment);
+        help = new HelpRequestDetails(location, numberOfPeople, doctorRequired, comment);
+    }
+
+    //Adds the data to the table in azure infrastructure
+    @SuppressLint("StaticFieldLeak")
+    public void addData() {
+        new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    mHelpRequestTable.insert(help).get(); //Inserts data
+                } catch (Exception exception) {
+                    Log.e(TAG, exception.toString());
+                }
+                return null;
+            }
+        }.execute();
+
+        mLocationEdit.setText("");
+        mNumberOfPeopleEdit.setText("");
+        mDoctorRequiredCheck.setSelected(false);
+        mCommentEdit.setText("");
     }
 }
--- app/src/main/java/com/example/hp/elpis/HelpRequestDetails.java
+++ app/src/main/java/com/example/hp/elpis/HelpRequestDetails.java
@@ -1,14 +1,27 @@
 package com.example.hp.elpis;
 
+import com.microsoft.windowsazure.mobileservices.table.DateTimeOffset;
+
 //Contains information about the Help Request
 public class HelpRequestDetails {
+    @com.google.gson.annotations.SerializedName("id")
+    private String mId;
+    @com.google.gson.annotations.SerializedName("createdAt")
+    private DateTimeOffset mCreatedAt;
+    @com.google.gson.annotations.SerializedName("updatedAt")
+    private DateTimeOffset mUpdatedAt;
+    @com.google.gson.annotations.SerializedName("version")
+    private String mVersion;
+    @com.google.gson.annotations.SerializedName("location")
     private String mLocation;
+    @com.google.gson.annotations.SerializedName("numberOfPeople")
     private int mNumberOfPeople;
+    @com.google.gson.annotations.SerializedName("doctorRequired")
     private boolean mDoctorRequired;
+    @com.google.gson.annotations.SerializedName("comment")
     private String mComment;
 
-    public HelpRequestDetails(String location, int numberOfPeople, boolean doctorRequired, String comment)
-    {
+    public HelpRequestDetails(String location, int numberOfPeople, boolean doctorRequired, String comment) {
         mLocation = location;
         mNumberOfPeople = numberOfPeople;
         mDoctorRequired = doctorRequired;
@@ -16,19 +29,69 @@ public class HelpRequestDetails {
 
     }
 
+    public String getId() {
+        return mId;
+    }
+
+    public final void setId(String id) {
+        mId = id;
+    }
+
+    public DateTimeOffset getCreatedAt() {
+        return mCreatedAt;
+    }
+
+    protected void setCreatedAt(DateTimeOffset createdAt) {
+        mCreatedAt = createdAt;
+    }
+
+    public DateTimeOffset getUpdatedAt() {
+        return mUpdatedAt;
+    }
+
+    protected void setUpdatedAt(DateTimeOffset updatedAt) {
+        mUpdatedAt = updatedAt;
+    }
+
+    public String getVersion() {
+        return mVersion;
+    }
+
+    public final void setVersion(String version) {
+        mVersion = version;
+    }
+
     public String getLocation() {
         return mLocation;
     }
 
-    public boolean isDoctorRequired() {
-        return mDoctorRequired;
+    public final void setLocation(String location) {
+        mLocation = location;
     }
 
     public int getNumberOfPeople() {
         return mNumberOfPeople;
     }
 
+    public final void setNumberOfPeople(int numberOfPeople) {
+        mNumberOfPeople = numberOfPeople;
+    }
+
+    public boolean getDoctorRequired() {
+        return mDoctorRequired;
+    }
+
+    public final void setDoctorRequired(boolean doctorRequired) {
+        mDoctorRequired = doctorRequired;
+    }
+
     public String getComment() {
         return mComment;
     }
+
+    public final void setComment(String comment) {
+        mComment = comment;
+    }
+
+
 }
